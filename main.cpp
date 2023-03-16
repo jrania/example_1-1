83acbdd961e5cdc3ed312e35236017cc79d447ff
jrania <jrania@fi.uba.ar>
2023-03-16 23:44:42 +0000

    Message

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-    DigitalIn gasDetector(D3);
+    DigitalIn gasDetector(D2);
 
     DigitalOut alarmLed(LED1);
 
     gasDetector.mode(PullDown);
 
     alarmLed = OFF;
-
+    printf("%s\n", "Hello World");
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
