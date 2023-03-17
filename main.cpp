0965cfe4df1213b4f4e2e6ec49a5493da969e0d2
jrania <jrania@fi.uba.ar>
2023-03-17 00:21:56 +0000

    Message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
+/**
+* @file main.cpp
+* @author Sandoval, Raña
+* @date 16/03/2023
+* @brief Testear una entrada y una salida digital del NUCLEO-F429ZI
+*/
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+/**
+* @brief Monitorea el sensor de gas. Si lee un 1 enciende un LED
+*       Antes de iniciar imprime el mensaje "Hello World"
+*/
+
 int main()
 {
+    /**
+    * @brief Declarar las variables correspondientes a un pin digital de entrada (gasDetector)
+    *       y a un pin digital de salida (alarmLed).
+    * @param D2 
+    **/
     DigitalIn gasDetector(D2);
 
     DigitalOut alarmLed(LED1);
