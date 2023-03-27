ec408aedc4b7ff46fd33263b336fc8552eddab73
jrania <jrania@fi.uba.ar>
2023-03-27 22:44:04 +0000

    Message

--- main.cpp
+++ main.cpp
@@ -2,31 +2,44 @@
 * @file main.cpp
 * @author Sandoval, Raña
 * @date 16/03/2023
-* @brief Testear una entrada y una salida digital del NUCLEO-F429ZI
+* @section TP 2 B
+* @brief Analizar el código propuesto por la cátedra, detallando las estructuras
+*       características de C++. La propuesta es reemplazar el código para que el
+*       lenguaje utilizado sea C.
 */
 
+//=====[Librerias]=============================================================
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+//=====[Funcion Main]==========================================================
 /**
-* @brief Monitorea el sensor de gas. Si lee un 1 enciende un LED
-*       Antes de iniciar imprime el mensaje "Hello World"
+*   @brief  Monitorea el sensor de gas. Si lee un 1 enciende un LED
+*           Antes de iniciar imprime el mensaje "Hello World"
+*   @param  Ninguno
+*   @return Ninguno
 */
 
 int main()
 {
     /**
-    * @brief Declarar las variables correspondientes a un pin digital de entrada (gasDetector)
-    *       y a un pin digital de salida (alarmLed).
-    * @param D2 
-    **/
+    * Se inicializan dos estructuras gpio_t: gasDetector y alarmLed. Cada una
+    * corresponde a un pin de entrada/salida del NUCLEO.
+    * La guncion gpio_init_in() inicializa el pin D2 como entrada, asociandolo
+    * con gasDetector. La funcion gpio_init_out_ex() inicializa el pin LED1
+    * asociándolo con alarmLed y configurándolo como Pull Dowm.
+    */
     gpio_t gasDetector;
     gpio_init_in(&gasDetector, D2);
 
     gpio_t alarmLed;
     gpio_init_out_ex(&alarmLed, LED1, PullDown);
 
-
+    /**
+    * El loop while(true) testea los estados de gasDetector con la función gpio_red()
+    * que devuelve un 1 o un 0. En cada caso, escribe un 1 o un 0 en alarmLed con la
+    * función gpio_write().
+    */
     gpio_write(&alarmLed, OFF);
     printf("%s\n", "Hello World");
     while (true) {
